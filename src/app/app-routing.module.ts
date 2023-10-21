49e7c67d284284ef335183fb722d49eb351225e2
Davidhernandezn <d4vidhernandezn@gmail.com>
2023-10-20 22:31:06 -0600

    se agrega LazyLoading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  path: ''//LOCALHOST 4200
-];
+const routes: Routes = [{
+  //path: '',//LOCALHOST 4200
+  //component solo si son aplicaciones pequeñas
+  //LazyLoading
+  path:'',
+  loadChildren:() => import('./modules/home/home.module').then(m => m.HomeModule)   //Importación dinamica. con .then por ser promesa
+
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,8 +4,6 @@ import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
 
-
-
 @NgModule({
   declarations: [
     SideBarComponent,
