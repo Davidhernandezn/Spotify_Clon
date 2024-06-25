68aa5b290c2945a4f38e68962c68f3006920e0e0
Davidhernandezn <d4vidhernandezn@gmail.com>
2024-06-25 17:01:53 -0600

    Correccion de bug y levantamineto de APP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FavoritesPageComponent } from './modules/favorites/pages/favorites-page/favorites-page.component';
+//import { SharedModule } from "./shared/shared.module";
+import { FavoritesModule } from './modules/favorites/favorites.module';
 
 @NgModule({
-  declarations: [//Declaraciones, componentes, pipes
-    AppComponent, FavoritesPageComponent,
-  ],
-  imports: [//solo importar otros modulos
-    BrowserModule,
-    AppRoutingModule
-  ],
-
-  //exports:[], //EXPORTA LO QUE DECLARAS
-
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    //exports:[], //EXPORTA LO QUE DECLARAS
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FavoritesModule,
+       // SharedModule
+    ]
 })
 export class AppModule { }
